a798cd5bba4b36df9609e6ed4756e79234192019
motomux <motomux@gmail.com>
2016-11-19 03:38:54 -0800

    Add insertion sort

---
+++ sort/insertion_sort.go
@@ -0,0 +1,14 @@
+package main
+
+// InsertionSort sorts slice of int by insertion sort
+func InsertionSort(nums []int) {
+	for i := 1; i < len(nums); i++ {
+		for k := i; 0 < k; k-- {
+			if nums[k] < nums[k-1] {
+				nums[k], nums[k-1] = nums[k-1], nums[k]
+			} else {
+				continue
+			}
+		}
+	}
+}
