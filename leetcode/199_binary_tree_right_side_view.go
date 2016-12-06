3580009dd80cc93c2c1e85f4e1c79353bdc08a8c
motomux <motomux@gmail.com>
2016-12-06 01:24:40 -0800

    Add 199. Binary Tree Right Side View with list

--- leetcode/199_binary_tree_right_side_view.go
+++ leetcode/199_binary_tree_right_side_view.go
@@ -1,5 +1,7 @@
 package leetcode
 
+import "container/list"
+
 // 199. Binary Tree Right Side View
 func rightSideView(root *TreeNode) []int {
 	if root == nil {
@@ -19,6 +21,34 @@ func rightSideView(root *TreeNode) []int {
 			}
 		}
 		list = nextList
+	}
+
+	return res
+}
+
+// 199. Binary Tree Right Side View by container/list
+func rightSideView2(root *TreeNode) []int {
+	if root == nil {
+		return []int{}
+	}
+	res := []int{}
+	l := list.New()
+	l.PushBack(root)
+	for l.Len() > 0 {
+		front := l.Front().Value.(*TreeNode)
+		res = append(res, front.Val)
+		nextList := list.New()
+		for el := l.Front(); el != nil; el = l.Front() {
+			node := el.Value.(*TreeNode)
+			if node.Right != nil {
+				nextList.PushBack(node.Right)
+			}
+			if node.Left != nil {
+				nextList.PushBack(node.Left)
+			}
+			l.Remove(el)
+		}
+		l = nextList
 	}
 
 	return res
