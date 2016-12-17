fda5f5dcd53693745fece18c9e83106044938322
motomux <motomux@gmail.com>
2016-12-17 02:13:49 -0800

    Fix 136. Single Number with bit opperation

--- leetcode/136_single_number.go
+++ leetcode/136_single_number.go
@@ -2,20 +2,10 @@ package leetcode
 
 // 136. Single Number
 func singleNumber(nums []int) int {
-	m := make(map[int]int)
+	res := 0
 	for _, num := range nums {
-		if count, ok := m[num]; !ok {
-			m[num] = 1
-		} else {
-			m[num] = count + 1
-		}
+		res ^= num
 	}
 
-	for k, count := range m {
-		if count == 1 {
-			return k
-		}
-	}
-
-	return -1
+	return res
 }
