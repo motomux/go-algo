88925960862700a084a7f9b54f76465e1e389969
motomux <motomux@gmail.com>
2016-12-23 23:24:35 -0800

    Fix 246. Strobogrammatic Number

--- leetcode/246_strobogrammatic_number.go
+++ leetcode/246_strobogrammatic_number.go
@@ -2,26 +2,18 @@ package leetcode
 
 // 246. Strobogrammatic Number
 func isStrobogrammatic(num string) bool {
-	for i := 0; i < len(num)/2; i++ {
-		n1, n2 := num[i], num[len(num)-1-i]
-		switch {
-		case n1 == '9' && n2 == '6':
-		case n1 == '6' && n2 == '9':
-		case n1 != '8' && n1 != '1' && n1 != '0':
-			return false
-		case n1 != n2:
+	for i, j := 0, len(num)-1; i <= j; i, j = i+1, j-1 {
+		n := string([]byte{num[i], num[j]})
+		switch n {
+		case "00":
+		case "11":
+		case "88":
+		case "69":
+		case "96":
+		default:
 			return false
 		}
 	}
 
-	if len(num)%2 == 0 {
-		return true
-	}
-	m := num[len(num)/2]
-	switch {
-	case m == '0' || m == '1' || m == '8':
-		return true
-	default:
-		return false
-	}
+	return true
 }
